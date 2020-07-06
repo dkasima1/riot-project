47fb9833c9ef4fb5f0fdafef2db9382ea9b214e3
dkasima1@jhu.edu <dkasima1@jhu.edu>
2020-07-06 15:59:42 -0400

    Updates

---
+++ precache-manifest.1466349b6e80e69eb179f7abf37899a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7207b8b892cc2362ed84d91f0b76825d",
+    "url": "/riot-project/index.html"
+  },
+  {
+    "revision": "0410fd564e1714868f6d",
+    "url": "/riot-project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "29bea1e5672d9d43a256",
+    "url": "/riot-project/static/js/2.a569dcca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/riot-project/static/js/2.a569dcca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0410fd564e1714868f6d",
+    "url": "/riot-project/static/js/main.fb16414e.chunk.js"
+  },
+  {
+    "revision": "63dd79ef4354b80163d0",
+    "url": "/riot-project/static/js/runtime-main.2cce6fc1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/riot-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
