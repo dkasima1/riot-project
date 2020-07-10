3568f8de8077251ae60f6b18c6c7d5a8be73fe09
dkasima1@jhu.edu <dkasima1@jhu.edu>
2020-07-10 15:32:22 -0400

    center search bar

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,15 +1,37 @@
 import React from "react";
 import { MDBCol } from "mdbreact";
 import "./SearchPage.css";
+import { Button, FormControl, FormGroup, InputGroup, Container, Col } from 'react-bootstrap';
 
-const SearchPage = () => {
-  return (
-    <MDBCol md="6">
-      <div className="active-pink-3 active-pink-4 mb-4">
-        <input className="form-control" type="text" placeholder="Search" aria-label="Search" />
-      </div>
-    </MDBCol>
-  );
+class SearchPage extends React.Component {
+constructor(props) {
+  super(props);
+  
+  this.handleEnter = this.handleEnter.bind(this);
+}
+
+  render () {
+    return (
+      <Container>
+        <Col md={{span: 6, offset: 3}}>
+          <InputGroup size="lg" className="mb-3">
+            <FormControl
+              placeholder="Search Summoner"
+              aria-label="Summoner Name"
+              aria-describedby="basic-addon2"
+            />
+            <InputGroup.Append>
+              <Button onCLick={this.handleEnter.bind(this)} variant="outline-secondary">Button</Button>
+            </InputGroup.Append>
+          </InputGroup>
+        </Col>
+      </Container>
+    )      
+  }
+
+  handleEnter(e) {
+    alert("a key was pressed")
+  }
 }
 
 export default SearchPage;
