6630433994ef6bd429e50b6974bf495cf0ef1225
Ray <rlpwns49@gmail.com>
2020-07-13 13:26:05 -0500

    Leaderboards stuff

--- src/components/Leaderboards/Leaderboards.js
+++ src/components/Leaderboards/Leaderboards.js
@@ -2,9 +2,38 @@ import React from "react";
 
 class Leaderboards extends React.Component {
 
-    render() {
+	componentDidMount() {
+		fetch('./Challenger.json')
+		.then((res) => res.json())
+    .then((data) => {
+			data.entries.sort((a, b) => a.leaguePoints - b.leaguePoints);
+			this.setState({data: data});
+    });
+	}
 
-    }
+	render() {
+		return (
+			<div>
+				<br /><br />
+				<br /><br />
+
+				< table className="table">
+					<th>Name</th>
+					<th>LP</th>
+					<tbody>{this.state.data.entries.map(function (item) {
+						return (
+							<tr>
+								<td>{item.summonerName}</td>
+								<td>{item.leaguePoints}</td>
+							</tr>
+						)
+
+					})}</tbody>
+				</table>
+
+			</div>
+		)
+	}
 }
 
-export default Leaderboards;
+export default Leaderboards
