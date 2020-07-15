52bb543727fe3749149cb1f9441f890c2b456621
dkasima1@jhu.edu <dkasima1@jhu.edu>
2020-07-15 15:04:54 -0400

    champion icons

--- src/components/Champions/Champions.js
+++ src/components/Champions/Champions.js
@@ -1,10 +1,26 @@
 import React from "react";
+import ReactDOM from 'react-dom';
+
+var championIcons = []
 
 class Champions extends React.Component {
+    importAll(r) {
+        return r.keys().map(r);
+    }
+
+    componentWillMount() {
+        championIcons = this.importAll(require.context('./../../assets/10.14.1/img/champion', false, /\.(png|jpe?g|svg)$/))
+    }
 
     render() {
         return (
-            <div>Champions</div>
+            <div>
+                {
+                    championIcons.map(
+                        (image, index) => <img key={index} src={image} alt="info"></img>
+                    )
+                }       
+            </div>
         )
     }
 }
