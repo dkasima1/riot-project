59a899bd701838000e82247b351d8f610e295218
Ray <rlpwns49@gmail.com>
2020-07-22 13:45:04 -0500

    converted main NavBar into a class, added handleChange function for Search Bar

--- FrontEnd/src/components/NavigationBar.js
+++ FrontEnd/src/components/NavigationBar.js
@@ -1,37 +1,53 @@
 import React from 'react';
 import { Navbar, Nav, NavDropdown, Button, Form, FormControl } from 'react-bootstrap';
-import { Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-function NavigationBar() {
-	return (
-		<div>
-			<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-				<Navbar.Brand as={Link} to="/Home">Flame.gg</Navbar.Brand>
-				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
-				<Navbar.Collapse id="responsive-navbar-nav">
-					<Nav className="m-auto">
-						<Nav.Link as={Link} to="/Champions">Champions</Nav.Link>
-						<Nav.Link as={Link} to="/Leaderboards">Leaderboards</Nav.Link>
-						<Nav.Link as={Link} to="/Stats">Stats</Nav.Link>
-					</Nav>
-					<Navbar bg="dark" variant="dark">
-						<Form inline>
-							<FormControl type="text" placeholder="Search" className="mr-sm-2" />
-							<Button onClick={handleOnClick} variant="outline-info">Search</Button>
-						</Form>
-					</Navbar>
-					<Nav>
-						<Nav.Link as={Link} to="/Deets">More deets</Nav.Link>
-						<Nav.Link as={Link} to="/Memes">Memers here</Nav.Link>
-					</Nav>
-				</Navbar.Collapse>
-			</Navbar>
-		</div>
-	)
+class NavigationBar extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+			summoner: ""
+		}
+
+		this.handleChange = this.handleChange.bind(this)
+		this.handleOnClick = this.handleOnClick.bind(this)
+	}
+
+	render() {
+		return (
+			<div>
+				<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+					<Navbar.Brand as={Link} to="/Home">Flame.gg</Navbar.Brand>
+					<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+					<Navbar.Collapse id="responsive-navbar-nav">
+						<Nav className="m-auto">
+							<Nav.Link as={Link} to="/Champions">Champions</Nav.Link>
+							<Nav.Link as={Link} to="/Leaderboards">Leaderboards</Nav.Link>
+							<Nav.Link as={Link} to="/Stats">Stats</Nav.Link>
+						</Nav>
+						<Navbar bg="dark" variant="dark">
+							<Form inline>
+								<FormControl name="summoner" value={this.state.summoner} 
+									onChange={this.handleChange} placeholder="Search" className="mr-sm-2" />
+								<Button	onClick={this.handleOnClick} variant="outline-info"> Search</Button>
+							</Form>
+						</Navbar>
+						<Nav>
+							<Nav.Link as={Link} to="/Deets">More deets</Nav.Link>
+							<Nav.Link as={Link} to="/Memes">Memers here</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Navbar>
+			</div>
+		)
+	}
+	handleOnClick() {
+		alert("searched " + this.state.summoner);
+	}
+
+	handleChange(event) {
+		this.setState({summoner: event.target.value})
+	}
+	
 }
-
-function handleOnClick() {
-	alert("searched");
-}
-
 export default NavigationBar;
