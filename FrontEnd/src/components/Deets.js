b85c7d8cf166b5cf1cf0b8b22e6294f8a1092234
Ray <rlpwns49@gmail.com>
2020-07-22 13:26:08 -0500

    cleaned up console msgs in memes/deets, new package.json

--- FrontEnd/src/components/Deets.js
+++ FrontEnd/src/components/Deets.js
@@ -1,10 +1,6 @@
 import React from "react"
 
 class Deets extends React.Component {
-  constructor() {
-    super()
-  }
-
   render() {
     return (
       <div>
--- FrontEnd/src/components/Memes.js
+++ FrontEnd/src/components/Memes.js
@@ -1,17 +1,13 @@
 import React from "react"
 
 class Memes extends React.Component {
-  constructor() {
-    super()
-  }
-
   render() {
     return (
       <div>
-        <img src="https://vignette.wikia.nocookie.net/dat-meme-wiki/images/1/1b/5cbf352666fba.jpeg/revision/latest?cb=20190424003333" />
-        <img src="https://i.kym-cdn.com/editorials/icons/mobile/000/000/491/stonks2.jpg" />
-        <img src="https://i.imgflip.com/3le773.jpg" />
-        <img src="https://pics.me.me/who-would-win-teslas-stock-prices-one-puffy-boi-market-36156628.png" />
+        <img src="https://vignette.wikia.nocookie.net/dat-meme-wiki/images/1/1b/5cbf352666fba.jpeg/revision/latest?cb=20190424003333" alt="meep"/>
+        <img src="https://i.kym-cdn.com/editorials/icons/mobile/000/000/491/stonks2.jpg" alt="meep"/>
+        <img src="https://i.imgflip.com/3le773.jpg" alt="meep"/>
+        <img src="https://pics.me.me/who-would-win-teslas-stock-prices-one-puffy-boi-market-36156628.png" alt="meep"/>
       </div>
     )
   }
