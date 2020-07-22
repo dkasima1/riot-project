2c5bb12498b556e1c9d274b673db2318d19d1348
Bannu <dkasima1@jhu.edu>
2020-07-22 17:56:24 -0400

    added backend

---
+++ BackEnd/app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+var request = require('request')
+
+const app = express()
+const port = 3002
+
+const api_key_bannu = 'RGAPI-39992cbf-1465-4fcf-a8a4-669386f8e297'
+
+app.get('/Challenger', (req, res) => {
+    request('https://na1.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5?api_key=RGAPI-39992cbf-1465-4fcf-a8a4-669386f8e297', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.send(body);
+        }
+    })
+});
+
+app.get('/Summoner/:id', (req, res) => {
+    request('https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + req.params.id + '?api_key=RGAPI-39992cbf-1465-4fcf-a8a4-669386f8e297', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            body = JSON.parse(body)
+            request('https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/' + body.id + '?api_key=RGAPI-39992cbf-1465-4fcf-a8a4-669386f8e297', function (err, resp, bod) {
+                if (!err && resp.statusCode == 200) {
+                    res.send(bod);
+                }
+            })
+        }
+    })
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
