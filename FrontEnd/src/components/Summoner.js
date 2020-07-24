2f7aa4935d03e528b06b3f18c6b119965dbe580b
Ray <rlpwns49@gmail.com>
2020-07-24 02:29:45 -0500

    fixed updating/refreshing issue in Summoner

--- FrontEnd/src/components/Summoner.js
+++ FrontEnd/src/components/Summoner.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {Table} from "react-bootstrap";
+import { Table } from "react-bootstrap";
 
 class Summoner extends React.Component {
   constructor(props) {
@@ -8,14 +8,18 @@ class Summoner extends React.Component {
       data: [],
       loading: true
     }
+
+    this.fetchData = this.fetchData.bind(this)
   }
 
   componentDidMount() {
-    fetch('http://localhost:3001/Summoner/' + this.props.match.params.summoner)
-      .then((res) => res.json())
-      .then((summoner) => {
-        this.setState({ data: summoner.data, loading: false });
-      });
+    this.fetchData()
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.match.params.summoner !== this.props.match.params.summoner) {
+      this.fetchData()
+    }
   }
 
   render() {
@@ -30,31 +34,39 @@ class Summoner extends React.Component {
       )
     }
     return (
-      <Table striped bordered size="sm" className="leaderboardsTable">
-					<thead>
-						<tr>
-							<th>Queue</th>
-							<th>Tier</th>
-							<th>Rank</th>
-							<th>LP</th>
-							<th>Wins</th>
-              <th>Losses</th>
-						</tr>
-					</thead>
-					<tbody>{this.state.data.map(function (item, key) {
-						return (
-							<tr key={key}>
-								<td>{item.queueType}</td>
-								<td>{item.tier}</td>
-								<td>{item.rank}</td>
-                <td>{item.leaguePoints}</td>
-								<td>{item.wins}</td>
-								<td>{item.losses}</td>
-							</tr>
-						)
-					})}</tbody>
-				</Table>
+      <Table striped bordered size="sm" className="leaderboardsTable" key={this.state.data.summonerName}>
+        <thead>
+          <tr>
+            <th>Queue</th>
+            <th>Tier</th>
+            <th>Rank</th>
+            <th>LP</th>
+            <th>Wins</th>
+            <th>Losses</th>
+          </tr>
+        </thead>
+        <tbody>{this.state.data.map(function (item, key) {
+          return (
+            <tr key={key}>
+              <td>{item.queueType}</td>
+              <td>{item.tier}</td>
+              <td>{item.rank}</td>
+              <td>{item.leaguePoints}</td>
+              <td>{item.wins}</td>
+              <td>{item.losses}</td>
+            </tr>
+          )
+        })}</tbody>
+      </Table>
     )
+  }
+
+  fetchData() {
+    fetch('http://localhost:3001/Summoner/' + this.props.match.params.summoner)
+      .then((res) => res.json())
+      .then((summoner) => {
+        this.setState({ data: summoner.data, loading: false });
+      });
   }
 }
 
