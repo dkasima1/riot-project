a8d4ccdeb9198e3fb2bf3c9e2d08cd4c7e981d7a
dkasima1 <47903015+dkasima1@users.noreply.github.com>
2020-08-13 15:55:07 -0400

    Update app.js

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 const app = express()
 const port = 3002
 
-const api_key_bannu = 'RGAPI-102d55d0-c4a2-4354-a186-016f96b18101'
+const api_key_bannu = 'hidden'
 
 app.get('/Challenger', (req, res) => {
 	request('https://na1.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5?api_key=' + api_key_bannu, function (error, response, body) {
